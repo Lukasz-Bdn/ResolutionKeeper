8bd0579f204285063aaa49f00fc439e843027f2e
Lukaszbn <lukasz-budyn@wp.pl>
2018-03-25 13:18:43 +0000

    user registration and saving user to db

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,18 @@ router.get("/register", function(req,res) {
     res.render("register");
 });
 
+router.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username}); 
+    User.register(newUser, req.body.password, function(err,user) {
+        if(err) {
+            // req.flash("error", err.message);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            // req.flash("success", "Welcome to Yelp Camp " + user.username);
+            res.redirect("/");
+        });
+    });
+});
+
 module.exports = router;
