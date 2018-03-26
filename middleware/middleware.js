dfd70d381229061bec68de6f191a6abbded9dca5
Lukaszbn <lukasz-budyn@wp.pl>
2018-03-26 12:09:47 +0000

    username and password length validation added to html

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -6,6 +6,9 @@ middlewareObj.isRegistrationDataValid = function(req, res, next) {
     if(req.body.password.length < 5) {
         req.flash("error", "Password must be at least 6 characters long.");
         res.redirect("/register");
+    } else if (req.body.username.length < 5) {
+        req.flash("error", "Username must be at least 6 characters long.");
+        res.redirect("/register");
     } else if(req.body.password !== req.body.confirmPassword) {
         req.flash("error", "Please make sure both entered passwords match.");
         res.redirect("/register");
