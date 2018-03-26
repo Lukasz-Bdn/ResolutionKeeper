1d0835b3049b14aa2088524f541416d99ff21057
Lukaszbn <lukasz-budyn@wp.pl>
2018-03-26 15:38:08 +0000

    login, register, logout links visible only when needed

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var User = require("../models/user");
 var middleware = require("../middleware/middleware");
 
 router.get("/", function(req, res) {
-    console.log(middleware);
     res.render("landing");
 });
 
@@ -17,14 +16,31 @@ router.post("/register", middleware.isRegistrationDataValid, function(req, res) 
     var newUser = new User({username: req.body.username, email: req.body.email});
     User.register(newUser, req.body.password, function(err,user) {
         if(err) {
-            // req.flash("error", err.message);
+            req.flash("error", err.message);
             return res.render("register");
         }
         passport.authenticate("local")(req, res, function() {
-            // req.flash("success", "Welcome to Yelp Camp " + user.username);
+            req.flash("success", "Welcome to Resolution Keeper " + user.username);
             res.redirect("/");
         });
     });
+});
+
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
+router.post("/login", passport.authenticate("local", 
+    {
+        failureRedirect: "/login",
+        successRedirect: "/"
+    }), function(req, res) {
+});
+
+router.get("/logout", function(req, res) {
+   req.logout();
+   req.flash("success", "Logged you out, thanks for visiting.");
+   res.redirect("/");
 });
 
 module.exports = router;
